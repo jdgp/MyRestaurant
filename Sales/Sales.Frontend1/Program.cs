697f5e26bbb8586cc3ad6b41e8020728e428093b
JDGP <juangil2574447@correo.itm.edu.co>
2023-09-05 22:06:02 -0500

    Ajuste

--- Sales/Sales.Frontend1/Program.cs
+++ Sales/Sales.Frontend1/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Sales.Frontend;
 using Sales.Frontend.Repositories;
+using Sales.Frontend1;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7048/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
