7048664f8210b3f9a95b693e4bbe7c43abb819a2
JDGP <juangil2574447@correo.itm.edu.co>
2023-09-06 00:37:43 -0500

    complete country edit

--- Sales/Sales.Frontend1/Program.cs
+++ Sales/Sales.Frontend1/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.Frontend1;
 using Sales.Frontend.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7048/") });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
