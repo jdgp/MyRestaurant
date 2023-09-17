4f7f57e952644f986a70b0c30044dfe95e24c249
JDGP <juangil2574447@correo.itm.edu.co>
2023-09-17 17:18:12 -0500

    Crud multi nivel

--- Sales/Sales.Backend/Controllers/StatesController.cs
+++ Sales/Sales.Backend/Controllers/StatesController.cs
@@ -1,12 +1,39 @@
-﻿using Sales.Backend.Interfaces;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.Backend.Data;
+using Sales.Backend.Interfaces;
 using Sales.Shared.Entities;
 
 namespace Sales.Backend.Controllers
 {
     public class StatesController : GenericController<State>
     {
-        public StatesController(IGenericUnitOfWork<State> unitOfWork) : base(unitOfWork)
+
+        private readonly DataContext _context;
+        public StatesController(IGenericUnitOfWork<State> unitOfWork, DataContext context) : base(unitOfWork)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.States
+                .Include(s => s.Cities)
+                .ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var state = await _context.States
+                .Include(s => s.Cities)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return Ok(state);
         }
     }
 }
--- Sales/Sales.Backend/Data/DataContext.cs
+++ Sales/Sales.Backend/Data/DataContext.cs
@@ -10,14 +10,21 @@ namespace Sales.Backend.Data
             
         }
 
-        public DbSet<Country> Countries { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<State> States { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>() .HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
             modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<State>().HasIndex(s => new { s.Name, s.CountryId }).IsUnique();
+            modelBuilder.Entity<City>().HasIndex(c => new { c.Name, c.StateId }).IsUnique();
         }
     }
 }
